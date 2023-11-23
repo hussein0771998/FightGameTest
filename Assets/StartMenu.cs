78cd99e19b19619e00bc452e5b4154c6cba12be1
hussein0771998 <115999088+hussein0771998@users.noreply.github.com>
2023-11-23 07:57:32 +0300

    start animation

--- Assets/StartMenu.cs
+++ Assets/StartMenu.cs
@@ -4,19 +4,38 @@ using UnityEngine;
 using GameAnalyticsSDK;
 using TMPro;
 using UnityEngine.SceneManagement;
+using DG.Tweening;
+using UnityEngine.UI;
+
 public class StartMenu : MonoBehaviour
 {
     // Start is called before the first frame update
     public string UserName;
     int level ;
     public TMP_InputField enterNameInputField;
-   
+    public DOTweenAnimation tweenAnimation;
+    bool playTweenOnce;
+    float timer;
+    public Image loadingBar;
     void Start()
     {
-        level = 1;
-       GameAnalytics.Initialize();
+           timer = 0f;
+           playTweenOnce = true;
+           level = 1;
+           GameAnalytics.Initialize();
     }
+    private void Update()
+    {
+        timer += Time.deltaTime;
+        loadingBar.fillAmount = timer / 4f;
+        if (timer >= 4)
+        {
+            
+            tweenAnimation.DOPlay();
+        }
+       
 
+    }
     // Update is called once per frame
     public void SetName_Play()
     {
